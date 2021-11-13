9b1f42aaf4796d165c070f8aabe5e48604b74d68
Michał Broniszewski <michal.broniszewski@mint.net.pl>
2021-11-14 00:03:53 +0100

    DB connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const dbConnectionUri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 mongoose.connect(
-  dbConnectionUri,
-  {
-    useNewUrlParser: true,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASSWORD,
-    authSource: "admin"
-  },
-  (error) => {
-    if (error) {
-      console.error.bind(console, `connection error: ${error}`)
+    process.env.DB_CONNECTION_STRING,
+    {
+        useNewUrlParser: true,
+        authSource: "admin"
+    },
+    (error) => {
+        if (error) {
+            console.error.bind(console, `connection error: ${error}`)
+        }
+        console.log(`Connected successfully: ${process.env.DB_CONNECTION_STRING}`)
     }
-    console.log(`Connected successfully: ${dbConnectionUri}`)
-  })
+)
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const securityRouter = require('./routers/security')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.APP_PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
